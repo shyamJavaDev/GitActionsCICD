fa929454c28851a957cc7f343633bdb536906e35
Shyambabu_Ch <Shyambabu_Ch@II39-GFLBNN2.mhf2.mhf.mhc>
2020-08-10 10:46:32 +0530

    commiting cicd projcet

---
+++ src/main/java/poc/GitActionCICD.java
@@ -0,0 +1,11 @@
+package main.java.poc;
+
+public class GitActionCICD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to Git CICD");
+
+	}
+
+}
